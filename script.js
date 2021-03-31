6bd3b752486fc96d63a1cf2949dbf111d58182d8
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-03-31 14:10:00 -0400

    added var, array, and function to display weather data.

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var city = $("#city");
-var submit = $("#submit")
-var carTitle = $(".card-title")
-var temperature = $(".card-text")
-var dateTime = $(".date")
-
-
-
+var submit = $("#submit");
+var cardTitle = $(".card-title");
+var temperature = $(".temperature");
+var dateTime = $(".date");
+var weather = $(".conditions");
+var humid = $(".humidity");
+var wind = $(".windSpeed");
+var uv = $(".uvIndex");
 
 submit.on("click", function () {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast/?q=" + city.val() + "&mode=json&units=metric&appid=4f071de0ce28a47df5c272c2ae6d1d96"
@@ -16,40 +17,55 @@ submit.on("click", function () {
         return response.json();
     })
     .then(function (data) {
+        console.log(data)
         
         var date = [];
         var dailyTemp = [];
+        var conditions = [];
+        var humidity = [];
+        var windSpeed = [];
+        var uvIndex = [];
+
         for (var i = 0; i <data.list.length; i+= 8) {
             dailyTemp.push(data.list[i].main.temp);
+            
             var dateSplit = data.list[i].dt_txt.split(" "); 
             date.push(dateSplit[0]);
-        }
-        console.log(date)
-        carTitle.each(function (index, ele) {
-            carTitle.text(data.city.name); 
-        })
+            
+            conditions.push(data.list[i].weather[0].main);
+            
+            humidity.push(data.list[i].main.humidity);
+
+            windSpeed.push(data.list[i].wind.speed);
+        };
+        
+        console.log(conditions)
+
+        
+        cardTitle.each(function (index, ele) {
+            cardTitle.text(data.city.name); 
+        });
 
         dateTime.each(function (index, ele) {
             $(ele).text(date[index]);
-        })
+        });
 
         temperature.each(function (index, ele) {
-            $(ele).text(dailyTemp[index]);
+            $(ele).text(`Temp: ${dailyTemp[index]}`);
         });
+
+        weather.each(function (index, ele) {
+            $(ele).text(conditions[index]);
+        });
+
+        humid.each(function (index, ele) {
+            $(ele).text(`Humidity: ${humidity[index]}%`);
+        });
+
+        wind.each(function (index, ele) {
+            $(ele).text(`Wind Speed: ${windSpeed[index]}km/h`);
+        })
     })
 })    
 
 
-// for (var i = 0; i <data.list.length; i+=8) {
-//     temperature.each(function (index, ele) {
-//        $(ele).text(data.list[i].main.temp)
-//         console.log(data.list[i].main.temp)
-//     })
-// }
-
-// temperature.each(function (index, ele) {
-//     for (var i = 0; i < data.list.length; i+=8){
-//         $(ele).text(data.list[index].main.temp)
-//         console.log(data.list[index].main.temp)
-//     }
-// })
