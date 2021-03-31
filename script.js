a993c841a9fd7d414c607538be4dfeef5ee07dca
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-03-31 09:46:26 -0400

    added each function to set card title to city name.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var city = $("#city");
 var submit = $("#submit")
+var carTitle = $(".card-title")
+var temperature = $(".card-text")
 
-console.log(city)
 submit.on("click", function () {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city.val() + "&mode=json&units=metric&appid=4f071de0ce28a47df5c272c2ae6d1d96"
-
 
     fetch(requestUrl) 
     .then (function (response) {
@@ -13,11 +13,16 @@ submit.on("click", function () {
     })
     .then(function (data) {
         console.log(data)
-        console.log(data.city)
-        console.log(data.list[0].main)
-        console.log(data.list[0].rain)
-        console.log(data.list[0].weather)
-        console.log(data.list[0].wind)
+    
+        carTitle.each(function (index, ele) {
+            carTitle.text(data.city.name) 
+        })
+        for (var i = 0; i <data.list.length; i+=8) {
+            temperature.each(function (index, ele) {
+                temperature.text(data.list[i].main.temp)
+                console.log(data.list[i].main.temp)
+            })
+        }
         
     })
 })    
